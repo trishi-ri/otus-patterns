136619b9e3f9015364338d1a37af6ee280439abe
trishi-ri <trishi-ri@ya.ru>
2022-03-11 01:31:03 +0300

    solveQuadraticEquation

---
+++ src/app/utils/math.utils.spec.ts
@@ -0,0 +1,215 @@
+import { MathUtils } from './math.utils';
+
+describe('MathUtils', () => {
+  describe('solveQuadraticEquation', () => {
+    describe('поиск корней', () => {
+      it('x^2+1 = 0 => корней нет', () => {
+        const a = 1,
+          b = 0,
+          c = 1;
+
+        const result = MathUtils.solveQuadraticEquation(a, b, c);
+
+        expect(result).toEqual([]);
+      });
+
+      it('x^2-1 = 0 => два корня кратности 1 (x1=1, x2=-1)', () => {
+        const a = 1,
+          b = 0,
+          c = -1;
+
+        const result = MathUtils.solveQuadraticEquation(a, b, c);
+
+        expect(result).toEqual([1, -1]);
+      });
+
+      it('x^2+0.2x+0.01 = 0 => один корень кратности 2 (x1=x2=-0.1)', () => {
+        const a = 1,
+          b = 0.2,
+          c = 0.01;
+
+        const result = MathUtils.solveQuadraticEquation(a, b, c);
+
+        expect(result).toEqual([-0.1]);
+      });
+    });
+
+    describe('обработка исключений', () => {
+      it(`a = 0 => исключение 'коэффициент "a" не может быть равен 0'`, () => {
+        const a = 0,
+          b = 2,
+          c = 1;
+
+        expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+          'коэффициент "a" не может быть равен 0',
+        );
+      });
+
+      describe('все коэффициенты должны быть числами', () => {
+        const expectedError = (coefficient: string) =>
+          `коэффициент "${coefficient}" должен быть числом`;
+
+        [
+          {
+            coefficient: 'a',
+            args: [NaN, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, NaN, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, NaN],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = NaN => исключение '${expectedError(coefficient)}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+      });
+
+      describe('все коэффициенты должны быть конечными числами', () => {
+        const expectedError = (coefficient: string) =>
+          `коэффициент "${coefficient}" должен быть конечным числом`;
+
+        [
+          {
+            coefficient: 'a',
+            args: [Infinity, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, Infinity, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, Infinity],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = Infinity => исключение '${expectedError(coefficient)}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+
+        [
+          {
+            coefficient: 'a',
+            args: [-Infinity, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, -Infinity, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, -Infinity],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = -Infinity => исключение '${expectedError(coefficient)}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+      });
+
+      describe('все коэффициенты не должны быть граничными числами', () => {
+        const expectedError = (coefficient: string) =>
+          `коэффициент "${coefficient}" не должен быть граничным числом`;
+
+        [
+          {
+            coefficient: 'a',
+            args: [Number.MAX_VALUE, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, Number.MAX_VALUE, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, Number.MAX_VALUE],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = MAX_VALUE => исключение '${expectedError(coefficient)}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+
+        [
+          {
+            coefficient: 'a',
+            args: [Number.MIN_VALUE, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, Number.MIN_VALUE, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, Number.MIN_VALUE],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = MIN_VALUE => исключение '${expectedError(coefficient)}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+
+        [
+          {
+            coefficient: 'a',
+            args: [Number.MAX_SAFE_INTEGER, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, Number.MAX_SAFE_INTEGER, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, Number.MAX_SAFE_INTEGER],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = MAX_SAFE_INTEGER => исключение '${expectedError(
+            coefficient,
+          )}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+
+        [
+          {
+            coefficient: 'a',
+            args: [Number.MIN_SAFE_INTEGER, 1, 1],
+          },
+          {
+            coefficient: 'b',
+            args: [1, Number.MIN_SAFE_INTEGER, 1],
+          },
+          {
+            coefficient: 'c',
+            args: [1, 1, Number.MIN_SAFE_INTEGER],
+          },
+        ].forEach(({ coefficient, args: [a, b, c] }) => {
+          it(`${coefficient} = MIN_SAFE_INTEGER => исключение '${expectedError(
+            coefficient,
+          )}'`, () => {
+            expect(() => MathUtils.solveQuadraticEquation(a, b, c)).toThrowError(
+              expectedError(coefficient),
+            );
+          });
+        });
+      });
+    });
+  });
+});
---
+++ src/app/utils/math.utils.ts
@@ -0,0 +1,65 @@
+export class MathUtils {
+  public static solveQuadraticEquation(a: number, b: number, c: number): number[] {
+    if (isNaN(a)) {
+      throw new Error('коэффициент "a" должен быть числом');
+    }
+    if (isNaN(b)) {
+      throw new Error('коэффициент "b" должен быть числом');
+    }
+    if (isNaN(c)) {
+      throw new Error('коэффициент "c" должен быть числом');
+    }
+
+    if (!Number.isFinite(a)) {
+      throw new Error('коэффициент "a" должен быть конечным числом');
+    }
+    if (!Number.isFinite(b)) {
+      throw new Error('коэффициент "b" должен быть конечным числом');
+    }
+    if (!Number.isFinite(c)) {
+      throw new Error('коэффициент "c" должен быть конечным числом');
+    }
+
+    if (MathUtils.isBoundary(a)) {
+      throw new Error('коэффициент "a" не должен быть граничным числом');
+    }
+    if (MathUtils.isBoundary(b)) {
+      throw new Error('коэффициент "b" не должен быть граничным числом');
+    }
+    if (MathUtils.isBoundary(c)) {
+      throw new Error('коэффициент "c" не должен быть граничным числом');
+    }
+
+    if (MathUtils.equal(a, 0)) {
+      throw new Error('коэффициент "a" не может быть равен 0');
+    }
+
+    const d = b * b - 4 * a * c;
+
+    if (MathUtils.equal(d, 0)) {
+      const x = -b / (2 * a);
+      return [x];
+    }
+
+    if (d > 0) {
+      const x1 = (-b + Math.sqrt(d)) / (2 * a);
+      const x2 = (-b - Math.sqrt(d)) / (2 * a);
+      return [x1, x2];
+    }
+
+    return [];
+  }
+
+  public static equal(a: number, b: number): boolean {
+    return Math.abs(a - b) < Number.EPSILON;
+  }
+
+  public static isBoundary(value: number): boolean {
+    return [
+      Number.MAX_VALUE,
+      Number.MIN_VALUE,
+      Number.MAX_SAFE_INTEGER,
+      Number.MIN_SAFE_INTEGER,
+    ].includes(value);
+  }
+}
