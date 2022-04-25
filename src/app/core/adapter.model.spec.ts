10316908ce2f3503fc9954784a121062da6c893e
Asha Owl <trishi-ri@ya.ru>
2022-04-25 19:49:29 +0300

    adapter (#13)
    * adapter
    
    * change generation by generic to generation by eval
    
    * generation of adapter was moved into Adapter class

---
+++ src/app/core/adapter.model.spec.ts
@@ -0,0 +1,80 @@
+import { capture, spy } from 'ts-mockito';
+import { IoC } from '../app.config';
+import { Adapter, MetaData } from './adapter.model';
+import { Command } from './command.model';
+import { Movable, MoveCommand } from './movements/move.model';
+import { UObject } from './u-object.model';
+import { Vector } from './vector.model';
+
+describe('adapters', () => {
+  IoC.resolve<Command>('IoC.Register', 'Adapter', (interfaceName: string, uObject: UObject) =>
+    Adapter.generateAdapter(interfaceName, uObject),
+  ).execute();
+
+  it('генерация адаптера для Movable', () => {
+    const uObject: UObject = getUObject();
+    const adapter = IoC.resolve<Movable>('Adapter', 'Movable', uObject);
+
+    const move = new MoveCommand(adapter);
+    move.execute();
+    move.execute();
+    expect(adapter.getPosition()).toEqual(new Vector(2, 6));
+  });
+
+  it('генерация адаптера для интерфейса с дополнительным методом (не геттер и не сеттер)', () => {
+    const spiedConsole = spy(console);
+
+    const uObject: UObject = getUObject();
+    const adapter = IoC.resolve<MovableWithFinish>('Adapter', 'MovableWithFinish', uObject);
+    const finishCommand = new FinishCommand(adapter);
+    finishCommand.execute();
+
+    const [loggedMessage] = capture(spiedConsole.log).last();
+    expect(loggedMessage).toEqual('finish!');
+  });
+
+  function getUObject(): UObject {
+    IoC.resolve<Command>('IoC.Register', 'uObject.Position', () => new Vector(0, 0)).execute();
+    IoC.resolve<Command>('IoC.Register', 'uObject.Velocity', () => new Vector(1, 3)).execute();
+    return {
+      getProperty: <T>(key: string): T => {
+        return IoC.resolve<T>(`uObject.${key}`);
+      },
+      setProperty: <T>(key: string, newValue: T): void => {
+        IoC.resolve<Command>('IoC.Register', `uObject.${key}`, () => newValue).execute();
+      },
+    };
+  }
+});
+
+interface MovableWithFinish {
+  getPosition(): Vector;
+  setPosition(newPosition: Vector): void;
+  getVelocity(): Vector;
+  finish(): void;
+}
+
+IoC.resolve<Command>(
+  'IoC.Register',
+  'MovableWithFinish.Metadata',
+  () =>
+    ({
+      className: 'MovableWithFinish',
+      methods: ['getPosition', 'getVelocity', 'setPosition', 'finish'],
+      methodDefinitions: {
+        finish: () => console.log('finish!'),
+      },
+    } as MetaData<MovableWithFinish>),
+).execute();
+
+class FinishCommand implements Command {
+  constructor(private finishable: MovableWithFinish) {}
+
+  public execute(): void {
+    try {
+      this.finishable.finish();
+    } catch (e) {
+      throw e;
+    }
+  }
+}
---
+++ src/app/core/adapter.model.ts
@@ -0,0 +1,71 @@
+import { IoC } from '../app.config';
+import { KeyofAsTuple } from '../types';
+import { Command } from './command.model';
+import { Movable } from './movements/move.model';
+import { UObject } from './u-object.model';
+
+export type MetaData<T> = {
+  className: string;
+  methods: KeyofAsTuple<T>;
+  methodDefinitions?: Partial<T>;
+};
+
+export class Adapter {
+  private static readonly getterRegExp = /^get(\w+)$/;
+  private static readonly setterRegExp = /^set(\w+)$/;
+
+  public static generateAdapter(interfaceName: string, uObject: UObject) {
+    const metadata = IoC.resolve<MetaData<unknown>>(`${interfaceName}.Metadata`);
+    const adapter = eval(Adapter.getClassDefenition(metadata));
+    return new adapter(uObject, IoC);
+  }
+
+  private static getMethodDefenition<T>(
+    methodName: string,
+    className: string,
+    methodDefinitions?: Partial<T>,
+  ): string {
+    const isGetter = Adapter.getterRegExp.test(methodName);
+    const isSetter = Adapter.setterRegExp.test(methodName);
+    if (isGetter || isSetter) {
+      const match = methodName.match(isGetter ? Adapter.getterRegExp : Adapter.setterRegExp);
+      const property = match ? match[1] : methodName;
+      if (isGetter) {
+        const getterKey = `${className}:${property}.get`;
+        IoC.resolve<Command>('IoC.Register', getterKey, (obj: UObject) =>
+          obj.getProperty(property),
+        ).execute();
+        return `${methodName}() { return this.IoC.resolve("${getterKey}", this.uObject)}`;
+      } else {
+        const setterKey = `${className}:${property}.set`;
+        IoC.resolve<Command>('IoC.Register', setterKey, (obj: UObject, newValue: unknown) =>
+          obj.setProperty(property, newValue),
+        ).execute();
+        return `${methodName}(newValue){return this.IoC.resolve("${setterKey}", this.uObject, newValue)}`;
+      }
+    } else if (methodDefinitions?.hasOwnProperty(methodName)) {
+      const otherKey = `${className}:${methodName}`;
+      IoC.resolve<Command>('IoC.Register', otherKey, () =>
+        eval(`methodDefinitions.${methodName}()`),
+      ).execute();
+      return `${methodName}(){return this.IoC.resolve("${otherKey}", this.uObject)}`;
+    }
+    return '';
+  }
+
+  private static getClassDefenition<T>(metadata: MetaData<T>): string {
+    return `(class ${metadata.className}Adapter {
+      uObject; IoC;
+      constructor(uObject, IoC) {this.uObject = uObject;this.IoC = IoC;}
+      ${(metadata.methods as string[])
+        .map((name) => {
+          return Adapter.getMethodDefenition<T>(
+            name,
+            metadata.className,
+            metadata.methodDefinitions,
+          );
+        })
+        .join('\n')}
+      })`;
+  }
+}
