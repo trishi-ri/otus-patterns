1835bea91bc195e05e095d8b80a35060249a004b
trishi-ri <trishi-ri@ya.ru>
2022-05-27 22:10:56 +0300

    change return for tryInScope

--- src/app/core/ioc/ioc.utils.ts
+++ src/app/core/ioc/ioc.utils.ts
@@ -2,22 +2,20 @@ import { IoC } from 'src/app/app.config';
 import { Command } from '../command.model';
 
 export class IoCUtils {
-  public static tryInScope(scopeName: string, fn: Function): Error[] {
+  public static tryInScope<T>(scopeName: string, fn: () => T): T | Error | void {
     const previousScope = IoC.scopeName;
-    const catchedErrors = [];
     try {
       if (!IoC.scoupesNames.includes(scopeName)) {
         IoC.resolve<Command>('Scope.New', scopeName).execute();
       }
       IoC.resolve<Command>('Scope.Current', scopeName).execute();
-      fn();
+      return fn();
     } catch (e) {
       if (e instanceof Error) {
-        catchedErrors.push(e);
+        return e;
       }
     } finally {
       IoC.resolve<Command>('Scope.Current', previousScope).execute();
     }
-    return catchedErrors;
   }
 }
